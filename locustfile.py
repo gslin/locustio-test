9330b0a345e5a6dc7f9cec35866c285e97b483ed
Gea-Suan Lin <gslin@gslin.org>
2019-05-17 17:08:50 +0800

    Import a basic version of locustfile.py.

---
+++ locustfile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from locust import HttpLocust, TaskSet, task
+
+class WebsiteTasks(TaskSet):
+    @task
+    def index(self):
+        self.client.get('/')
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    min_wait = 5000
+    max_wait = 15000
